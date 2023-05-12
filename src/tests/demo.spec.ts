b7aac0a83ec934e51a87545309e14899326c0d78
dnuma-eip <dnuma@evolveip.net>
2023-05-12 17:03:09 -0400

    Framework setup finished

---
+++ src/tests/demo.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from "@playwright/test";
+import { USER_INFO } from "@fixtures/config";
+
+// Slow down the test for better visualization
+test.use({launchOptions: {slowMo: 300 }});
+
+test.beforeEach(async ({ page }) => {
+  
+
+});
+
+test.describe('Navigate on WorkRamp website',
+  () => {
+    test('', async ({ page })  => {
+          
+    });
+
+  });
